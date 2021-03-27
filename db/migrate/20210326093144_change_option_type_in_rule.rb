5abef249dd0b15705c2b5fe4151c59dc34bd9f38
root <peterjson17199@gmail.com>
2021-03-27 14:33:37 +0700

    ducnh commit

---
+++ db/migrate/20210326093144_change_option_type_in_rule.rb
@@ -0,0 +1,5 @@
+class ChangeOptionTypeInRule < ActiveRecord::Migration[6.0]
+  def change
+	change_column :snort_rules, :options, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_133631) do
+ActiveRecord::Schema.define(version: 2021_03_26_093144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -164,7 +164,7 @@ ActiveRecord::Schema.define(version: 2021_02_05_133631) do
     t.string "dest_address"
     t.string "dest_port"
     t.boolean "status", default: true
-    t.jsonb "options", default: {}
+    t.text "options", default: "{}"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["action"], name: "index_snort_rules_on_action"
